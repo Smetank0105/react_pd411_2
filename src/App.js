a818b232a39b5aa943fd99472c54895642f87d9b
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-02-13 19:34:49 +0700

    removePost DONE

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ import Range from './Range.js';
 import Form from './Form.js';
 import Converter from './Converter.js';
 import Posts from './posts/Posts.js';
+import React from 'react';
 
-let posts = 
-[
-  {id:'1', title:'C++ proc', content:'Процедурное программирование на языке С++'},
-  {id:'2', title:'C++ OOP', content:'Объектно-ориентированое программирование на языке С++'},
-  {id:'3', title:'Windows Desktop Development', content:'Разработка настольных приложений для ОС Windows'}
-]
-function removePost(id)
-{
-  alert(id);
-  posts = posts.filter(post => post.id != id);
-}
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Header />
+  state =
+    {
+      posts:
+        [
+          { id: '1', title: 'C++ proc', content: 'Процедурное программирование на языке С++' },
+          { id: '2', title: 'C++ OOP', content: 'Объектно-ориентированое программирование на языке С++' },
+          { id: '3', title: 'Windows Desktop Development', content: 'Разработка настольных приложений для ОС Windows' }
+        ]
+    }
+
+  removePost = (id) => {
+    alert(id);
+    this.setState({posts: this.state.posts.filter(post => post.id !== id)});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Header />
       <Hello />
       <Length />
       <Range />
       <Form />
       <Converter /> */}
-      <Posts posts={posts} removePost={removePost} />
-    </div>
-  );
-}
+        <Posts posts={this.state.posts} removePost={this.removePost} />
+      </div>
+    );
+  }
 
+}
 export default App;
